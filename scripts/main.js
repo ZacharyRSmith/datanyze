b794cbdb110bb0ec44f22ee473da01b51d19c669
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-02-09 21:41:39 -0800

    (feat) Render commands with JSPlumb line

---
+++ scripts/main.js
@@ -0,0 +1,65 @@
+window.layoutCommands = function (session, commands) {
+  // These arguments are ensured to be objects of the structures:
+  // var session = {
+  //   startTime:123456789, // timestamp
+  //   endTime:133456999 // timestamp
+  // };
+  // var commands = [
+  //   { timestamp:123456799, // timestamp
+  //     commandName:'Lock', // human readable command
+  //     commandType:'lock' // fontawesome name without fa­
+  //   },
+  //   {
+  //     timestamp:123456889,
+  //     commandName:'Chat',
+  //     commandType:'comment­o'
+  //   }
+  // ];
+  commands.sort(function (a, b) {
+    return b.timestamp - a.timestamp;
+  });
+  commands.forEach(function (command) {
+    command.timestamp = formatTime(command.timestamp);
+  });
+
+  function appendClassBoundary (time, text) {
+    $('#schedule').append(
+      '<div class="command-container">' +
+        '<div class="column-left time">' + time + '</div>' +
+        '<span class="class-boundary column-center fa-stack fa-lg">' +
+          '<i class="fa fa-circle-thin fa-stack-2x"></i>' +
+          '<i class="fa fa-lock fa-stack-1x"></i>' +
+        '</span>' +
+        '<div class="column-right">' + text + '</div>' +
+      '</div>'
+    );
+  }
+
+  function appendCommand (command) {
+    $('#schedule').append(
+      '<div class="command-container">' +
+        '<div class="column-left time">' + command.timestamp + '</div>' +
+        '<span class="column-center fa-stack fa-lg">' +
+          '<i class="fa fa-circle fa-stack-2x"></i>' +
+          '<i class="fa fa-circle-thin fa-stack-2x"></i>' +
+          '<i class="fa fa-lock fa-stack-1x"></i>' +
+        '</span>' +
+        '<div class="column-right">' + command.commandName + '</div>' +
+      '</div>'
+    );
+  }
+
+  function formatTime (timestamp) {
+    return moment.unix(timestamp).format("h.mma");
+  }
+
+  var endTime = formatTime(session.endTime);
+  appendClassBoundary(endTime, "Class session ended");
+
+  commands.forEach(function (command) {
+    appendCommand(command);
+  });
+
+  var startTime = formatTime(session.startTime);
+  appendClassBoundary(startTime, "Class session started");
+};
